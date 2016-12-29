4125868ef1dcb9930d2823d1ee685898fc4e84d5
No-bo-dy <pencho1234567@gmail.com>
2016-12-29 08:14:38 +0200

    Class Submission Created
    Created Class Submission - added members - user, task, sourceCode,
    sourceLang

---
+++ Backup/ASP.NET-Sample/StudentSystem/SudentSystemClient.cs
@@ -0,0 +1,42 @@
+﻿//
+//  SudentSystemClient.cs
+//
+//  Author:
+//       Alex Tsvetanov <alex@tsalex.tk>
+//
+//  Copyright (c) 2016 Alex Tsvetanov
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System.Collections.Generic;
+
+namespace StudentSystem
+{
+	class Client
+	{
+		static public Dictionary <string, User> Users = new Dictionary<string, User> ();
+		static public List <Track> Tracks = new List<Track> ();
+		static Client ()
+		{
+			Users ["alex@tsalex.tk"] = new User ("Alex Tsvetanov", "alex@tsalex.tk", "coconutmasters");
+			Tracks.Add (new Track (
+									"C#", 
+									"./courses/cs.aspx",
+									new List<Course> () {
+										new Course ("Intro", "./cs/intro.aspx")
+									}
+			                      )
+			           );
+		}
+	}
+}
---
+++ Backup/ASP.NET-Sample/StudentSystem/User.cs
@@ -0,0 +1,45 @@
+﻿//
+//  User.cs
+//
+//  Author:
+//       Alex Tsvetanov <alex@tsalex.tk>
+//
+//  Copyright (c) 2016 Alex Tsvetanov
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace StudentSystem
+{
+	public class User : IComparable<User>
+	{
+		public string name { get; private set; }
+		public string email { get; private set; }
+		public string password { get; private set; }
+		public int points { get; private set; }
+		public User (string Name, string Email, string Password)
+		{
+			name = Name;
+			email = Email;
+			password = Password.GetHashCode ().ToString ();
+			points = 0;
+		}
+
+		public int CompareTo (User obj) 
+		{
+			return email.CompareTo (obj.email);
+		}
+	}
+}
+
